e605322fa43ebbac4869e1ceee5c87d3d7e65304
olga-kri <krivenkoolga1987@gmail.com>
2024-02-24 00:28:22 +0500

    Homework without test

---
+++ src/main/java/DontFindElementException.java
@@ -0,0 +1,20 @@
+public class DontFindElementException extends RuntimeException {
+    public DontFindElementException() {
+    }
+
+    public DontFindElementException(String message) {
+        super(message);
+    }
+
+    public DontFindElementException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DontFindElementException(Throwable cause) {
+        super(cause);
+    }
+
+    public DontFindElementException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
